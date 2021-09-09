dd282e8035a6e20573b2228f912dbc3fe178dc44
albiriku <albiriku@example.com>
2021-09-09 21:45:34 +0200

    Update files

--- main.py
+++ main.py
@@ -1,3 +1,156 @@
+#!/usr/bin/env python
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import json
+import shutil
+
+import ansible.constants as C
+from ansible.executor.task_queue_manager import TaskQueueManager
+from ansible.module_utils.common.collections import ImmutableDict
+from ansible.inventory.manager import InventoryManager
+from ansible.parsing.dataloader import DataLoader
+from ansible.playbook.play import Play
+from ansible.plugins.callback import CallbackBase
+from ansible.vars.manager import VariableManager
+from ansible import context
+
+
+# Create a callback plugin so we can capture the output
+class ResultsCollectorJSONCallback(CallbackBase):
+    """A sample callback plugin used for performing an action as results come in.
+
+    If you want to collect all results into a single object for processing at
+    the end of the execution, look into utilizing the ``json`` callback plugin
+    or writing your own custom callback plugin.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(ResultsCollectorJSONCallback, self).__init__(*args, **kwargs)
+        self.host_ok = {}
+        self.host_unreachable = {}
+        self.host_failed = {}
+
+    def v2_runner_on_unreachable(self, result):
+        host = result._host
+        self.host_unreachable[host.get_name()] = result
+
+    def v2_runner_on_ok(self, result, *args, **kwargs):
+        """Print a json representation of the result.
+
+        Also, store the result in an instance attribute for retrieval later
+        """
+        host = result._host
+        self.host_ok[host.get_name()] = result
+        print(json.dumps({host.name: result._result}, indent=4))
+
+    def v2_runner_on_failed(self, result, *args, **kwargs):
+        host = result._host
+        self.host_failed[host.get_name()] = result
+
+
+def run_playbook(config, ip, event, model):
+    host_list = ['131.226.217.143'] 
+    # since the API is constructed for CLI it expects certain options to always be set in the context object
+    context.CLIARGS = ImmutableDict(connection='smart', module_path=['/home/albiriku/devnet/dne-dna-code/venv-flask/lib/python3.8/site-packages/ansible/modules'], forks=10, verbosity=True, check=False, diff=False)
+
+    # become=None, become_method=None, become_user=None, check=False, diff=False)
+
+    """
+    # required for
+    # https://github.com/ansible/ansible/blob/devel/lib/ansible/inventory/manager.py#L204
+    sources = ','.join(host_list)
+    if len(host_list) == 1:
+        sources += ','
+    """
+
+    # initialize needed objects
+    loader = DataLoader() #Takes care of finding and reading yaml, json and ini files
+    passwords = dict(vault_pass='secret')
+
+    # Instantiate our ResultsCollectorJSONCallback for handling results as they come in. Ansible expects this to be one of its main display outlets
+    results_callback = ResultsCollectorJSONCallback()
+
+    # create inventory, use path to host config file as source or hosts in a comma separated string
+    inventory = InventoryManager(loader=loader, sources='/home/albiriku/devnet/dne-dna-code/intro-ansible/hosts') #sources
+
+    # variable manager takes care of merging all the different sources to give you a unified view of variables available in each context
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+
+    # instantiate task queue manager, which takes care of forking and setting up all objects to iterate over host list and tasks
+    # IMPORTANT: This also adds library dirs paths to the module loader
+    # IMPORTANT: and so it must be initialized before calling `Play.load()`.
+    tqm = TaskQueueManager(
+        inventory=inventory,
+        variable_manager=variable_manager,
+        loader=loader,
+        passwords=passwords,
+        stdout_callback=results_callback,  # Use our custom callback instead of the ``default`` callback plugin, which prints to stdout
+    )
+
+    # create data structure that represents our play, including tasks, this is basically what our YAML loader does internally.
+    config = {"ietf-interfaces:interface":{"name": "Loopback104","description": "Added with RESTCONF","type": "iana-if-type:softwareLoopback","enabled": True,"ietf-ip:ipv4":{"address":[{"ip": "172.16.100.10","netmask": "255.255.255.0"}]}}}
+
+    config_put = {"ietf-interfaces:interface:":{"name": "Loopback104","description": "PUT with RESTCONF - YES", "type": "iana-if-type:softwareLoopback","type": "iana-if-type:softwareLoopback","enabled": False,"ietf-ip:ipv4":{"address":[{"ip": "172.16.100.69","netmask": "255.255.255.0"}]}}}
+
+    config_patch = {"ietf-interfaces:interface:":{"enabled": True}}
+
+    json_object = json.dumps(config, indent = 4)
+    print(json_object)
+
+    play_source = dict(
+        name="Ansible Play",
+        hosts=host_list,
+        gather_facts='no',
+        tasks=[
+            dict(action=dict(module='ansible.netcommon.restconf_config', args=dict(path='/data/ietf-interfaces:interfaces', content=json_object, method='post')))
+            #dict(action=dict(module='ansible.netcommon.restconf_config', args=dict(path='/data/ietf-interfaces:interfaces/interface=Loopback104', content=json_object, method='put')))
+            #dict(action=dict(module='ansible.netcommon.restconf_config', args=dict(path='/data/ietf-interfaces:interfaces/interface=Loopback104', content=json_object, method='patch')))
+            #dict(action=dict(module='ansible.netcommon.restconf_config', args=dict(path='/data/ietf-interfaces:interfaces/interface=Loopback104', method='delete')))
+            #dict(action=dict(module='ansible.netcommon.restconf_get', args=dict(path='/data/ietf-interfaces:interfaces', content='config')))
+            
+
+            #dict(action=dict(module='debug', args=dict(msg='{{result.candidate}}')))
+            #dict(action=dict(module='shell', args='ls'), register='shell_out'),
+            #dict(action=dict(module='debug', args=dict(msg='{{shell_out.stdout}}'))),
+            #dict(action=dict(module='command', args=dict(cmd='/usr/bin/uptime'))),
+        ]
+    )
+
+
+    # Create play object, playbook objects use .load instead of init or new methods,
+    # this will also automatically create the task objects from the info provided in play_source
+    play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
+
+    # Actually run it
+    try:
+        result = tqm.run(play)  # most interesting data for a play is actually sent to the callback's methods
+    finally:
+        # we always need to cleanup child procs and the structures we use to communicate with them
+        tqm.cleanup()
+        if loader:
+            loader.cleanup_all_tmp_files()
+
+    # Remove ansible tmpdir
+    shutil.rmtree(C.DEFAULT_LOCAL_TMP, True)
+
+    print("UP ***********")
+    for host, result in results_callback.host_ok.items():
+        print('{0} >>> {1}'.format(host, result._result['candidate'])) #candidate
+
+    print("FAILED *******")
+    for host, result in results_callback.host_failed.items():
+        print('{0} >>> {1}'.format(host, result._result['msg']))
+
+    print("DOWN *********")
+    for host, result in results_callback.host_unreachable.items():
+        print('{0} >>> {1}'.format(host, result._result['msg']))
+
+
+
+
+
 from flask import Flask, request, Response      #used for flask app, receive and response of webhook
 import json                                     #used for handling json objects
 import requests                                 #used for HTTP get request to netbox api
@@ -55,17 +208,20 @@ or returns a value of None
 """
 def pick_out_values(model, data, values):
     config = {}
+
     for element in list_of_models[model]["configurable"]:
         if element in values:
             config["configurable"] = {}
             #Key is added in the dictionary "config" along with a value
             config["configurable"][element] = values[element]
     
-
-    if "configurable" in config == True:
+    print('1',config)
+    if "configurable" in config != None:
         config["informational"] = {}
         info = list_of_models[model]["informational"]
         length = len(info)
+
+        print('2',config)
 
         for i in range(0, length):                                      #loop på antalet element i info
             if i == 0:                                                      #första varvet:
@@ -98,7 +254,7 @@ def get_api_data(config):
     else:
         return
 
-
+"""
 def create_playbook(config, ip, device_name):
     #timestamp
     dateTimeObj = datetime.now()
@@ -121,7 +277,7 @@ def create_playbook(config, ip, device_name):
     fil.write("hej")
     fil.close()
     return
-
+"""
 """
 Below is the flask app code that receives the webhook.
 Calls the functions responsible for each step.
@@ -174,7 +330,8 @@ def respond():
 
     if config == None:
         print()
-        print("config == None. No configurable values were returned from the 'pick_out_values' function, or")
+        print("config == None. No configurable values were returned from the 'pick_out_values' function,")
+        print("device has no primary IP assigned to it, or")
         print("assigned_objects contains a value of null. This occurs when a webhook is triggered for an ipaddress which has not been assigned to a device")
         print("no configuration needed, because the change doesnt relate to a device.")
         return Response(status=200)
@@ -193,6 +350,6 @@ def respond():
             return Response(status=200)
 
     #step 5: playbook
-    create_playbook(config, ip, device_name)
+    run_playbook(config, ip, event, model) #device_name
 
     return Response(status=200)
